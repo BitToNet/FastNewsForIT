ba64c997d5a8a3f7f71a05a77c6ea2d36480b509
邱珑 <502578360@qq.com>
2019-03-06 10:11:58 +0800

    测试首页适合的效果

--- app/src/main/java/cn/bittonet/news/adapter/GridShopAdapter.java
+++ app/src/main/java/cn/bittonet/news/adapter/GridShopAdapter.java
@@ -1,98 +1,24 @@
 package cn.bittonet.news.adapter;
 
-import android.support.annotation.Nullable;
-import android.support.v4.content.ContextCompat;
 import cn.bittonet.news.R;
 import cn.bittonet.news.base.holder.BaseViewHolderHelper;
-import cn.bittonet.news.bean.DataListBean;
-import cn.bittonet.news.bean.RepoAreaBean;
-import cn.bittonet.news.bean.ShopBean;
 import com.chad.library.adapter.base.BaseQuickAdapter;
-import java.util.List;
+import java.util.ArrayList;
 
 /**
  * Created by Felone on 2019/2/22 0022 上午 10:30
  * description：
  * 50257836@qq.com
  */
-public class GridShopAdapter extends BaseQuickAdapter<ShopBean, BaseViewHolderHelper> {
-    private DataListBean dataListBean;
-    private RepoAreaBean repoArea;
+public class GridShopAdapter extends BaseQuickAdapter<Integer, BaseViewHolderHelper> {
 
-    public GridShopAdapter(@Nullable List<ShopBean> data) {
+    public GridShopAdapter(ArrayList<Integer> data) {
         super(R.layout.item_grid_shop, data);
     }
 
     @Override
-    protected void convert(BaseViewHolderHelper helper, ShopBean item) {
-        repoArea = item.getRepoArea();
-        if (item.getDataList().size() > 0) {
-            dataListBean = item.getDataList().get(0);
-        } else {
-            return;
-        }
-
-        helper.setImageUrl(R.id.iv, item.getRepoArea().getImageUrl())
-              .setText(R.id.tv, item.getRepoArea().getName());
-        if (dataListBean.getDevice().getIsConnect() == 0) {
-            helper.setText(R.id.tv_state, "未连接")
-                  .setTextColor(R.id.tv_state, ContextCompat.getColor(mContext, R.color.colorRed));
-        } else {
-            helper.setText(R.id.tv_state, "正常")
-                  .setTextColor(R.id.tv_state,
-                                ContextCompat.getColor(mContext, R.color.text_color_green));
-        }
-
-        if (dataListBean.getHumidity() != null && dataListBean.getTemperature() != null) {
-            helper.setValue(R.id.cp_temp, dataListBean.getTemperature().getValue(), 30)
-                  .setValue(R.id.cp_humi, dataListBean.getHumidity().getValue(), 30);
-            setTemp(helper);
-            sethumi(helper);
-        } else if (dataListBean.getHumidity() == null && dataListBean.getTemperature() == null) {
-        } else {
-            if (dataListBean.getTemperature() == null) {
-                helper.setValue(R.id.cp_humi, dataListBean.getHumidity().getValue(), 30);
-                sethumi(helper);
-            }
-            if (dataListBean.getHumidity() == null) {
-                helper.setValue(R.id.cp_temp, dataListBean.getTemperature().getValue(), 30);
-                setTemp(helper);
-            }
-        }
-    }
-
-    private void sethumi(BaseViewHolderHelper helper) {
-        //湿度在范围内，绿色
-        //湿度不在范围内，黄色；若设备已连接，状态字体和颜色也改为污染和黄色
-        if (dataListBean.getHumidity().getValue() > repoArea.getSaveStandard().getHumidityMax()
-                || dataListBean.getHumidity().getValue() < repoArea.getSaveStandard()
-                                                                   .getHumidityMin()) {
-            helper.setProgressColor(R.id.cp_humi,
-                                    ContextCompat.getColor(mContext, R.color.text_color_yellow))
-                  .setValue(R.id.cp_humi, dataListBean.getTemperature().getValue(), 60);
-            if (dataListBean.getDevice().getIsConnect() != 0) {
-                helper.setText(R.id.tv_state, "污染")
-                      .setTextColor(R.id.tv_state,
-                                    ContextCompat.getColor(mContext, R.color.text_color_yellow));
-            }
-        }
-    }
-
-    private void setTemp(BaseViewHolderHelper helper) {
-        //温度在范围内，绿色
-        //温度不在范围内，黄色；若设备已连接，状态字体和颜色也改为污染和黄色
-        if (dataListBean.getTemperature().getValue() > repoArea.getSaveStandard()
-                                                               .getTemperatureMax()
-                || dataListBean.getTemperature().getValue() < repoArea.getSaveStandard()
-                                                                      .getTemperatureMin()) {
-            helper.setProgressColor(R.id.cp_temp,
-                                    ContextCompat.getColor(mContext, R.color.text_color_yellow))
-                  .setValue(R.id.cp_temp, dataListBean.getTemperature().getValue(), 60);
-            if (dataListBean.getDevice().getIsConnect() != 0) {
-                helper.setText(R.id.tv_state, "污染")
-                      .setTextColor(R.id.tv_state,
-                                    ContextCompat.getColor(mContext, R.color.text_color_yellow));
-            }
-        }
+    protected void convert(BaseViewHolderHelper helper, Integer item) {
+//        helper.setImageUrl(R.id.video_image,"http://img.xspic.com/img8/39/123/2456359_1.jpg");
+        helper.setImageResource(R.id.video_image,item);
     }
 }
--- app/src/main/java/cn/bittonet/news/fragment/ShowingFragment.java
+++ app/src/main/java/cn/bittonet/news/fragment/ShowingFragment.java
@@ -1,12 +1,95 @@
 package cn.bittonet.news.fragment;
 
-import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.PagerSnapHelper;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import butterknife.BindView;
+import cn.bittonet.news.R;
+import cn.bittonet.news.adapter.GridShopAdapter;
+import cn.bittonet.news.base.fragment.BaseFragment;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import java.util.ArrayList;
 
 /**
  * Created by Felone on 2019/2/20 0020 下午 3:28
  * description：
  * 50257836@qq.com
  */
-public class ShowingFragment extends Fragment {
+public class ShowingFragment extends BaseFragment {
+
+
+    @BindView(R.id.recyclerview)
+    RecyclerView       recyclerview;
+    @BindView(R.id.smartrefresh)
+    SmartRefreshLayout refreshLayout;
+    @BindView(R.id.layout)
+    FrameLayout        layout;
+    @BindView(R.id.layout_bottom)
+    LinearLayout       layoutBottom;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_showing;
+    }
+
+    @Override
+    protected void initView() {
+
+        refreshLayout.setEnableLoadmore(false);
+        refreshLayout.setEnableRefresh(false);
+        recyclerview.setNestedScrollingEnabled(false);
+        PagerSnapHelper snapHelper = new PagerSnapHelper() {
+            // 在 Adapter的 onBindViewHolder 之后执行
+            @Override
+            public int findTargetSnapPosition(RecyclerView.LayoutManager layoutManager,
+                    int velocityX, int velocityY) {
+                //                // TODO 找到对应的Index
+                int targetPos = super.findTargetSnapPosition(layoutManager, velocityX, velocityY);
+                //                setMvItemClick(linearLayoutManager.findViewByPosition(targetPos));
+                //                if (targetPos >= (strings.size() - 1)) {
+                //                    strings.addAll(Arrays.asList(urls));
+                //                    mvAdapter.notifyDataSetChanged();
+                //                }
+                return targetPos;
+            }
+
+            // 在 Adapter的 onBindViewHolder 之后执行
+            @Nullable
+            @Override
+            public View findSnapView(RecyclerView.LayoutManager layoutManager) {
+                // TODO 找到对应的View
+                View view = super.findSnapView(layoutManager);
+                //                setMvItemClick(view);
+                return view;
+            }
+        };
+        snapHelper.attachToRecyclerView(recyclerview);
+
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerview.setLayoutManager(layoutManager);
+
+
+        ArrayList<Integer> objects = new ArrayList<>();
+        objects.add(R.drawable.bg_all);
+        objects.add(R.drawable.bg_half);
+        GridShopAdapter gridShopAdapter = new GridShopAdapter(objects);
+        recyclerview.setAdapter(gridShopAdapter);
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    protected View injectTarget() {
+        return layout;
+    }
 
 }
